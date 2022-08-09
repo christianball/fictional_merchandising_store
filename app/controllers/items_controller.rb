6a5ea4cd39d8aa26d307476c4dd83fa2f8cad3de
Christian Ball <christianball@hotmail.co.uk>
2022-08-09 23:51:58 +0100

    Only return desired attributes in response.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
 
-    render json: @items
+    render json: @items.map{ _1.as_json(only: [:id, :code, :name, :price]) }
   end
 
 end
--- spec/requests/list_items_spec.rb
+++ spec/requests/list_items_spec.rb
@@ -3,15 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Listing items', :type => :request do
 
   it 'lists all items with exactly the expected attributes' do
-    item = create(:item, code: 'ONE', name: 'One', price: 11.11)
+    item_one = create(:item, code: 'ONE', name: 'One', price: 11.11)
+    item_two = create(:item, code: 'TWo', name: 'Two', price: 22.22)
 
     expected_response = [
-      {
-        id: item.id,
-        code: item.code,
-        name: item.name,
-        price: item.price
-      }
+      item_one.as_json(only: [:id, :code, :name, :price]),
+      item_two.as_json(only: [:id, :code, :name, :price])
     ].to_json
 
     get '/items'
